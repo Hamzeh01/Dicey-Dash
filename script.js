241b0acc3f32da4980341bb3077a4aa76bb117b8
Husen Hamze <summer.00.projects@gmail.com>
2023-10-02 13:27:21 +0330

    add state variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+const playerElements = document.querySelectorAll(".player");
+const totalScoreElements = document.querySelectorAll(".score");
+const currentScoreElements = document.querySelectorAll(".current-score");
+
+const btnNew = document.querySelector(".btn--new");
+const btnDice = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
